994188bda484ea2773467c7d627eb3e9e83269cd
Diego Ugarte <diego.ugarte@outlook.com>
2020-11-25 16:38:59 +0900

    :recycle: Refactored vec implementation

--- src/vec/vec_array.zig
+++ src/vec/vec_array.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
+const mem = std.mem;
 
 pub fn VecArray(comptime T: type, comptime n: usize) type {
     if (@typeInfo(T) != .Int and @typeInfo(T) != .Float) {
-        @compileError("Expected numeric types, found '" ++ @typeName(T) ++ "'");
+        @compileError("Expected numeric type, found '" ++ @typeName(T) ++ "'");
     }
     return struct {
         data: [n]T = undefined,
@@ -15,26 +16,33 @@ pub fn VecArray(comptime T: type, comptime n: usize) type {
             return Self{};
         }
 
-        pub fn fromArray(array: [n]T) Self {
-            return Self{
-                .data = array,
-            };
-        }
-
-        pub fn fromSlice(slice: []T) Self {
-            std.debug.assert(slice.len == Self.len);
-            var new_vec_array = Self.new();
-            for (new_vec_array.data) |*d, i| {
-                d.* = slice[i];
+        pub fn setFromArray(self: *Self, array: [n]T) void {
+            for (self.data) |*value, i| {
+                value.* = array[i];
             }
-
-            return new_vec_array;
         }
 
-        pub fn fromVecArray(vec: Self) Self {
-            return Self{
-                .data = vec.data,
-            };
+        pub fn setFromSlice(self: *Self, slice: []T) void {
+            mem.copy(T, self.data[0..], slice);
+        }
+
+        pub fn setFromVec(self: *Self, vec: anytype) void {
+            const VecT = @TypeOf(vec);
+            if (@typeInfo(VecT) != .Struct) @compileError("Expected Vec, found '" ++ @typeName(VecT) ++ "'");
+            if (!@hasDecl(VecT, "DataT")) @compileError("Vec does not contain DataT declaration");
+            if (!@hasField(VecT, "data")) @compileError("Vec does not contain data field");
+            if (VecT.DataT != T) @compileError("Expected Vec of data type '" ++ @typeName(T) ++ "', found '" ++ @typeName(VecT) ++ "'");
+
+            mem.copy(T, self.data[0..], vec.data[0..]);
+        }
+
+        pub fn set(self: *Self, data: anytype) void {
+            switch (@typeInfo(@TypeOf(data))) {
+                .Pointer => self.setFromSlice(data),
+                .Array => self.setFromArray(data),
+                .Struct => self.setFromVec(data),
+                else => @compileError("Expected Slice, Array, or Vec, found '" ++ @typeName(@TypeOf(data)) ++ "'"),
+            }
         }
     };
 }
@@ -58,50 +66,82 @@ test "VecArray new" {
     testing.expect(@TypeOf(v.data) == [2]u8);
 }
 
-test "VecArray fromArray" {
-    const V = VecArray(u8, 2);
-    var v = V.fromArray(.{ 0, 1 });
+test "VecArray setFromArray" {
+    var v = VecArray(u8, 2).new();
+
+    v.setFromArray(.{ 0, 1 });
     testing.expectEqual([2]u8{ 0, 1 }, v.data);
 
-    const W = VecArray(f32, 3);
-    var w = W.fromArray(.{ -1.0, 1.5, 3.5 });
+    var w = VecArray(f32, 3).new();
+
+    w.setFromArray(.{ -1.0, 1.5, 3.5 });
     testing.expectEqual([3]f32{ -1.0, 1.5, 3.5 }, w.data);
 }
 
-test "VecArray fromSlice" {
-    const V = VecArray(u8, 2);
-
+test "VecArray setFromSlice" {
     var slice_1 = try testing.allocator.alloc(u8, 2);
     defer testing.allocator.free(slice_1);
     slice_1[0] = 2;
     slice_1[1] = 5;
 
-    var v = V.fromSlice(slice_1);
+    var v = VecArray(u8, 2).new();
+
+    v.setFromSlice(slice_1);
     testing.expectEqual([2]u8{ 2, 5 }, v.data);
 
     var slice_2 = try testing.allocator.alloc(u8, 2);
     slice_2[0] = 3;
     slice_2[1] = 6;
 
-    v = V.fromSlice(slice_2);
-    testing.expectEqual([2]u8{ 3, 6 }, v.data);
+    var w = VecArray(u8, 2).new();
+
+    w.setFromSlice(slice_2);
+    testing.expectEqual([2]u8{ 3, 6 }, w.data);
 
     slice_2[0] = 1;
     slice_2[1] = 2;
-    testing.expectEqual([2]u8{ 3, 6 }, v.data);
+    testing.expectEqual([2]u8{ 3, 6 }, w.data);
 
     testing.allocator.free(slice_2);
-    testing.expectEqual([2]u8{ 3, 6 }, v.data);
+    testing.expectEqual([2]u8{ 3, 6 }, w.data);
 }
 
-test "VecArray fromVecArray" {
-    const V = VecArray(u8, 2);
-    var v1 = V.fromArray(.{ 0, 1 });
-    var v2 = V.fromVecArray(v1);
-    testing.expectEqual(v1, v2);
+test "VecArray setFromVec" {
+    var v = VecArray(u8, 2).new();
+    v.setFromArray(.{ 0, 1 });
 
-    const W = VecArray(f32, 3);
-    var w1 = W.fromArray(.{ -1.0, 1.5, 3.5 });
-    var w2 = W.fromVecArray(w1);
-    testing.expectEqual(w1, w2);
+    var w = VecArray(u8, 2).new();
+    w.setFromVec(v);
+
+    testing.expectEqual(v, w);
+}
+
+test "VecArray set" {
+    var v = VecArray(u8, 2).new();
+    var array = [2]u8{ 0, 1 };
+
+    v.set(array);
+    testing.expectEqual([2]u8{ 0, 1 }, v.data);
+
+    var w = VecArray(u8, 2).new();
+    var slice = array[0..];
+
+    w.set(slice);
+    testing.expectEqual([2]u8{ 0, 1 }, w.data);
+
+    var x = VecArray(u8, 2).new();
+    var vec_array = VecArray(u8, 2).new();
+    vec_array.setFromArray(.{ 0, 1 });
+
+    x.set(vec_array);
+    testing.expectEqual([2]u8{ 0, 1 }, x.data);
+
+    var y = VecArray(u8, 2).new();
+    const VecSlice = @import("vec_slice.zig").VecSlice;
+    var vec_slice = try VecSlice(u8, 2).new().init(testing.allocator);
+    defer vec_slice.deinit();
+    vec_slice.set([2]u8{ 0, 1 });
+
+    y.set(vec_slice);
+    testing.expectEqualSlices(u8, ([2]u8{ 0, 1 })[0..], y.data[0..]);
 }
--- src/vec/vec_slice.zig
+++ src/vec/vec_slice.zig
@@ -4,7 +4,7 @@ const Allocator = mem.Allocator;
 
 pub fn VecSlice(comptime T: type, comptime n: usize) type {
     if (@typeInfo(T) != .Int and @typeInfo(T) != .Float) {
-        @compileError("Expected numeric types, found '" ++ @typeName(T) ++ "'");
+        @compileError("Expected numeric type, found '" ++ @typeName(T) ++ "'");
     }
     return struct {
         data: []T = undefined,
@@ -15,7 +15,11 @@ pub fn VecSlice(comptime T: type, comptime n: usize) type {
         pub const len = n;
         pub const Self = @This();
 
-        pub fn init(allocator: *Allocator) !Self {
+        pub fn new() Self {
+            return Self{};
+        }
+
+        pub fn init(self: Self, allocator: *Allocator) !Self {
             return Self{
                 .data = try allocator.alloc(T, n),
                 .allocator = allocator,
@@ -30,37 +34,31 @@ pub fn VecSlice(comptime T: type, comptime n: usize) type {
             }
         }
 
-        pub fn realloc(self: *Self) !void {
-            if (self.allocated) {
-                self.allocator.free(self.data);
-            } else {
-                self.allocated = true;
+        pub fn setFromArray(self: *Self, array: [n]T) void {
+            mem.copy(T, self.data, array[0..]);
+        }
+
+        pub fn setFromSlice(self: *Self, slice: []T) void {
+            mem.copy(T, self.data, slice);
+        }
+
+        pub fn setFromVec(self: *Self, vec: anytype) void {
+            const VecT = @TypeOf(vec);
+            if (@typeInfo(VecT) != .Struct) @compileError("Expected Vec, found '" ++ @typeName(VecT) ++ "'");
+            if (!@hasDecl(VecT, "DataT")) @compileError("Vec does not contain DataT declaration");
+            if (!@hasField(VecT, "data")) @compileError("Vec does not contain data field");
+            if (VecT.DataT != T) @compileError("Expected Vec of data type '" ++ @typeName(T) ++ "', found '" ++ @typeName(VecT) ++ "'");
+
+            mem.copy(T, self.data, vec.data[0..]);
+        }
+
+        pub fn set(self: *Self, data: anytype) void {
+            switch (@typeInfo(@TypeOf(data))) {
+                .Pointer => self.setFromSlice(data),
+                .Array => self.setFromArray(data),
+                .Struct => self.setFromVec(data),
+                else => @compileError("Expected Slice, Array, or Vec, found '" ++ @typeName(@TypeOf(data)) ++ "'"),
             }
-            self.data = try self.allocator.alloc(T, n);
-        }
-
-        pub fn initFromArray(allocator: *Allocator, array: [n]T) !Self {
-            var new_vec_slice = try init(allocator);
-            for (new_vec_slice.data) |*value, i| {
-                value.* = array[i];
-            }
-
-            return new_vec_slice;
-        }
-
-        pub fn initFromSlice(allocator: *Allocator, slice: []T) !Self {
-            std.debug.assert(slice.len == Self.len);
-            var new_vec_slice = try init(allocator);
-            mem.copy(T, new_vec_slice.data, slice);
-
-            return new_vec_slice;
-        }
-
-        pub fn initFromVecSlice(allocator: *Allocator, vec_slice: Self) !Self {
-            var new_vec_slice = try init(allocator);
-            mem.copy(T, new_vec_slice.data, vec_slice.data);
-
-            return new_vec_slice;
         }
     };
 }
@@ -74,12 +72,21 @@ test "VecSlice" {
     const W = VecSlice(f32, 3);
     testing.expect(W.DataT == f32);
     testing.expect(W.len == 3);
+
+    const A = VecSlice(i32, 5);
+    const B = VecSlice(i32, 5);
+    testing.expect(A == B);
+}
+
+test "VecSlice new" {
+    const V = VecSlice(u8, 2);
+    var v = V.new();
+    testing.expect(@TypeOf(v) == V);
+    testing.expect(@TypeOf(v.data) == []u8);
 }
 
 test "VecSlice init" {
-    const V = VecSlice(u8, 2);
-
-    var v = try V.init(testing.allocator);
+    var v = try VecSlice(u8, 2).new().init(testing.allocator);
     defer v.allocator.free(v.data);
 
     testing.expect(v.data.len == 2);
@@ -88,94 +95,106 @@ test "VecSlice init" {
 }
 
 test "VecSlice deinit" {
-    const V = VecSlice(u8, 2);
-
-    var v = try V.init(testing.allocator);
+    var v = try VecSlice(u8, 2).new().init(testing.allocator);
 
     v.deinit();
 
     testing.expect(v.allocator == testing.allocator);
     testing.expect(v.allocated == false);
+
+    var w = VecSlice(u8, 2).new();
+
+    w.deinit();
+
+    testing.expect(w.allocated == false);
 }
 
-test "VecSlice realloc" {
-    const V = VecSlice(u8, 2);
-
-    var v = try V.init(testing.allocator);
+test "VecSlice setFromArray" {
+    var v = try VecSlice(u8, 2).new().init(testing.allocator);
     defer v.deinit();
 
-    v.data[0] = 0;
-    v.data[1] = 1;
+    v.setFromArray(.{ 0, 1 });
+    testing.expectEqualSlices(u8, ([2]u8{ 0, 1 })[0..], v.data);
 
-    try v.realloc();
-
-    testing.expect(v.data[0] != 0);
-    testing.expect(v.data[1] != 1);
-}
-
-test "VecSlice initFromArray" {
-    const V = VecSlice(u8, 2);
-
-    var v = try V.initFromArray(testing.allocator, .{ 0, 1 });
-    defer v.deinit();
-
-    testing.expect(mem.eql(u8, ([2]u8{ 0, 1 })[0..], v.data));
-
-    const W = VecSlice(f32, 3);
-
-    var w = try W.initFromArray(testing.allocator, .{ -1.0, 1.5, 3.5 });
+    var w = try VecSlice(f32, 3).new().init(testing.allocator);
     defer w.deinit();
 
-    testing.expect(mem.eql(f32, ([3]f32{ -1.0, 1.5, 3.5 })[0..], w.data));
+    w.setFromArray(.{ -1.0, 1.5, 3.5 });
+    testing.expectEqualSlices(f32, ([3]f32{ -1.0, 1.5, 3.5 })[0..], w.data);
 }
 
-test "VecSlice initFromSlice" {
-    const V = VecSlice(u8, 2);
-
+test "VecSlice setFromSlice" {
     var slice_1 = try testing.allocator.alloc(u8, 2);
     defer testing.allocator.free(slice_1);
     slice_1[0] = 2;
     slice_1[1] = 5;
 
-    var v = try V.initFromSlice(testing.allocator, slice_1);
+    var v = try VecSlice(u8, 2).new().init(testing.allocator);
     defer v.deinit();
 
-    testing.expect(mem.eql(u8, ([2]u8{ 2, 5 })[0..], v.data));
+    v.setFromSlice(slice_1);
+    testing.expectEqualSlices(u8, ([2]u8{ 2, 5 })[0..], v.data);
 
     var slice_2 = try testing.allocator.alloc(u8, 2);
     slice_2[0] = 3;
     slice_2[1] = 6;
 
-    v.deinit();
-    v = try V.initFromSlice(testing.allocator, slice_2);
-    testing.expect(mem.eql(u8, ([2]u8{ 3, 6 })[0..], v.data));
+    var w = try VecSlice(u8, 2).new().init(testing.allocator);
+    defer w.deinit();
+
+    w.setFromSlice(slice_2);
+    testing.expectEqualSlices(u8, ([2]u8{ 3, 6 })[0..], w.data);
 
     slice_2[0] = 1;
     slice_2[1] = 2;
-    testing.expect(mem.eql(u8, ([2]u8{ 3, 6 })[0..], v.data));
+    testing.expectEqualSlices(u8, ([2]u8{ 3, 6 })[0..], w.data);
 
     testing.allocator.free(slice_2);
-    testing.expect(mem.eql(u8, ([2]u8{ 3, 6 })[0..], v.data));
+    testing.expectEqualSlices(u8, ([2]u8{ 3, 6 })[0..], w.data);
 }
 
-test "VecSlice initFromVecSlice" {
-    const V = VecSlice(u8, 2);
+test "VecSlice setFromVec" {
+    var v = try VecSlice(u8, 2).new().init(testing.allocator);
+    defer v.deinit();
+    v.setFromArray(.{ 0, 1 });
 
-    var v1 = try V.initFromArray(testing.allocator, .{ 0, 1 });
-    defer v1.deinit();
+    var w = try VecSlice(u8, 2).new().init(testing.allocator);
+    defer w.deinit();
+    w.setFromVec(v);
 
-    var v2 = try V.initFromVecSlice(testing.allocator, v1);
-    defer v2.deinit();
+    testing.expectEqualSlices(u8, v.data, w.data);
+}
 
-    testing.expect(mem.eql(u8, v1.data, v2.data));
+test "VecSlice set" {
+    var v = try VecSlice(u8, 2).new().init(testing.allocator);
+    defer v.deinit();
+    var array = [2]u8{ 0, 1 };
 
-    const W = VecSlice(f32, 3);
+    v.set(array);
+    testing.expectEqualSlices(u8, ([2]u8{ 0, 1 })[0..], v.data);
 
-    var w1 = try W.initFromArray(testing.allocator, .{ -1.0, 1.5, 3.5 });
-    defer w1.deinit();
+    var w = try VecSlice(u8, 2).new().init(testing.allocator);
+    defer w.deinit();
+    var slice = array[0..];
 
-    var w2 = try W.initFromVecSlice(testing.allocator, w1);
-    defer w2.deinit();
+    w.set(slice);
+    testing.expectEqualSlices(u8, ([2]u8{ 0, 1 })[0..], w.data);
 
-    testing.expect(mem.eql(f32, w1.data, w2.data));
+    var x = try VecSlice(u8, 2).new().init(testing.allocator);
+    defer x.deinit();
+    var vec_slice = try VecSlice(u8, 2).new().init(testing.allocator);
+    defer vec_slice.deinit();
+    vec_slice.setFromArray(.{ 0, 1 });
+
+    x.set(vec_slice);
+    testing.expectEqualSlices(u8, ([2]u8{ 0, 1 })[0..], x.data);
+
+    var y = try VecSlice(u8, 2).new().init(testing.allocator);
+    defer y.deinit();
+    const VecArray = @import("vec_array.zig").VecArray;
+    var vec_array = VecArray(u8, 2).new();
+    vec_array.set([2]u8{ 0, 1 });
+
+    y.set(vec_array);
+    testing.expectEqualSlices(u8, ([2]u8{ 0, 1 })[0..], y.data);
 }
